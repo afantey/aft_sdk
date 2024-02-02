2bc30d9d6ac447eb6f9557bda5773f4dba015e45
rgw <rgw5267@gmail.com>
2024-02-02 23:57:59 +0800

    change sdk_crypto_decrypt_cbc param

--- src/components/crypto/sdk_crypto.h
+++ src/components/crypto/sdk_crypto.h
@@ -7,6 +7,6 @@
 #define __SDK_CRYPTO_H__
 
 int sdk_crypto_encrypt_cbc(const uint8_t *in, int inlen, uint8_t *out, int *outlen, const uint8_t *iv, const uint8_t *key);
-int sdk_crypto_decrypt_cbc(const uint8_t *in, int inlen, uint8_t *out, int *outlen, const uint8_t *iv, const uint8_t *key);
+int sdk_crypto_decrypt_cbc(const uint8_t *in, int inlen, uint8_t *out, const uint8_t *iv, const uint8_t *key);
 
 #endif //__SDK_CRYPTO_H__
