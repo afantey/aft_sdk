8eadb79f19537c82a13bc04ad7300d521682343f
rgw <rgw5267@gmail.com>
2024-03-11 16:13:16 +0800

    add example_flash

--- examples/example_flash.c
+++ examples/example_flash.c
@@ -1,29 +1,66 @@
+/**
+ * Change Logs:
+ * Date           Author       Notes
+ * {data}         rgw          first version
+ */
+#include "aft_sdk.h"
+#include "sdk_uart.h"
+#include "sdk_board.h"
+#include "fal.h"
 
-#define FLASH_USER_START_ADDR   (FLASH_BASE + FLASH_PAGE_SIZE * 512)             /* Start @ of user Flash area */
-#define FLASH_USER_END_ADDR     (FLASH_USER_START_ADDR + FLASH_PAGE_SIZE * 10)   /* End @ of user Flash area */
+#define DBG_LVL DBG_LOG
+#define DBG_TAG "test_flash"
+#include "sdk_log.h"
 
-uint8_t data[4] = {1, 2, 3, 4};
-uint32_t data32;
+#define TEST_DATA_SIZE  1024
 
-uint32_t Address = 0, PAGEError = 0;
-Address = FLASH_USER_START_ADDR;
-onchip_flash.ops.erase(&onchip_flash, FLASH_USER_START_ADDR, FLASH_PAGE_SIZE * 10);
-while (Address < FLASH_USER_END_ADDR)
+uint8_t write_buf[TEST_DATA_SIZE];
+uint8_t read_buf[TEST_DATA_SIZE];
+
+int main(void)
 {
-    if (onchip_flash.ops.write(&onchip_flash, Address, data, 4) > 0)
-    {
-        Address = Address + 4;
-    }
-    else
-    {
-        break;
-    }
-}
-Address = FLASH_USER_START_ADDR;
-while (Address < FLASH_USER_END_ADDR)
-{
-    data32 = *(__IO uint32_t *)Address;
+    board_init();
+    fal_init();
+    LOG_D("hello example flash");
 
-    Address = Address + 4;
-    LOG_D("%x", data32);
+    const struct fal_partition *fal_part = fal_partition_find("primary");
+    if (fal_part == NULL) {
+        LOG_E("partition not found");
+        return -1;
+    }
+
+    // 测试擦除
+    LOG_D("erase flash");
+    fal_partition_erase(fal_part, 0, fal_part->len);
+
+    // 测试写入
+    LOG_D("write data to flash");
+    memset(write_buf, 0x55, TEST_DATA_SIZE);
+    int ret = fal_partition_write(fal_part, 0, write_buf, TEST_DATA_SIZE);
+    if (ret < 0) {
+        LOG_E("write flash failed");
+        return -1;
+    }
+
+    // 测试读取
+    LOG_D("read data from flash");
+    ret = fal_partition_read(fal_part, 0, read_buf, TEST_DATA_SIZE);
+    if (ret < 0) {
+        LOG_E("read flash failed");
+        return -1;
+    }
+
+    // 判断写入和读取数据是否正确
+    LOG_D("compare data");
+    int i;
+    for (i = 0; i < TEST_DATA_SIZE; i++) {
+        if (write_buf[i] != read_buf[i]) {
+            LOG_E("data not match");
+            return -1;
+        }
+    }
+
+    LOG_D("test success");
+
+    return 0;
 }
