e06835eafc02024d1941c63108fff4dc102e6f32
rgw <rgw5267@gmail.com>
2024-04-12 09:35:04 +0800

    sdk_storage

---
+++ src/sdk_storage.c
@@ -0,0 +1,18 @@
+/**
+ * Change Logs:
+ * Date           Author          Notes
+ * 2024-04-09     rgw             first version
+ */
+
+#include "sdk_storage.h"
+#include "fdb.h"
+
+int sdk_stroage_init(void)
+{
+    return 0;
+}
+
+int sdk_stroage_put(uint32_t index, uint32_t len, void *content)
+{
+    
+}
